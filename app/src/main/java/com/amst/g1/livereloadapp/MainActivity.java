8a63fba1e9322a567bd10eab52f8bbb21f78d0b0
Miguel Sanchez <mangelsr@live.com>
2019-12-21 15:57:12 -0500

    [INIT]

---
+++ app/src/main/java/com/amst/g1/livereloadapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.amst.g1.livereloadapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
